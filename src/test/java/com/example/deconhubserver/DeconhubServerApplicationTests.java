de4877ddfe0d51c8d1c430d124110f5289323c47
jaemin <sonjm3518@dsm.hs.kr>
2022-09-26 08:54:15 +0900

    ♻️ :: setting

---
+++ src/test/java/com/example/deconhubserver/DeconhubServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deconhubserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeconhubServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
