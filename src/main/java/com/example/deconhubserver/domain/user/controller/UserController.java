a71092d95eddcd81550b8da20e97911d8ae9b85f
beargame123 <hoyoung7827@gmail.com>
2022-10-04 22:40:08 +0900

    ➕:: 로그인,회원가입, 토큰 재발급

---
+++ src/main/java/com/example/deconhubserver/domain/user/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.example.deconhubserver.domain.user.controller;
+
+import com.example.deconhubserver.domain.user.dto.LoginRequest;
+import com.example.deconhubserver.domain.user.dto.SignupRequest;
+import com.example.deconhubserver.domain.user.dto.UserResponse;
+import com.example.deconhubserver.domain.user.service.UserService;
+import com.example.deconhubserver.global.security.auth.AuthDetails;
+import com.example.deconhubserver.global.security.jwt.JwtTokenProvider;
+import com.example.deconhubserver.global.security.jwt.dto.TokenResponse;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @PostMapping("/signup")
+    public UserResponse login(@RequestBody SignupRequest request){
+        return userService.signup(request);
+    }
+
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody LoginRequest request)throws JsonProcessingException {
+        UserResponse userResponse = userService.login(request);
+        return jwtTokenProvider.createTokenByLogin(userResponse);
+    }
+
+    @GetMapping("/reissue")
+    public TokenResponse reissue(
+            @AuthenticationPrincipal AuthDetails authDetails
+    ) throws JsonProcessingException {
+        UserResponse userResponse = UserResponse.of(authDetails.getUser());
+        return jwtTokenProvider.reissueAtk(userResponse);
+    }
+    
+}
