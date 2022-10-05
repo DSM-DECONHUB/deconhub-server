9661dbb968d44198a259574d85530d0e1efcf264
beargame123 <hoyoung7827@gmail.com>
2022-10-05 11:26:11 +0900

    ➕:: jwtProvider 추가

--- src/main/java/com/example/deconhubserver/global/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/example/deconhubserver/global/security/jwt/JwtTokenProvider.java
@@ -1,58 +1,100 @@
 package com.example.deconhubserver.global.security.jwt;
 
 import com.example.deconhubserver.domain.user.dto.UserResponse;
-import com.example.deconhubserver.global.security.jwt.dto.Subject;
+import com.example.deconhubserver.global.security.jwt.dao.RedisDao;
 import com.example.deconhubserver.global.security.jwt.dto.TokenResponse;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.time.Duration;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @Component
 @Slf4j
 public class JwtTokenProvider {
 
-    private final ObjectMapper objectMapper;
+    private final UserDetailsService userDetailsService;
+    private final RedisDao redisDao;
 
     @Value("${spring.jwt.key}")
     private String key;
 
-    private final Long atkTime = 60 * 60 * 1000L;
+    private static final Long atkTime = 10 * 1000L;
+    private static final Long rtkTime = 7 * 24 * 60 *  60 * 1000L;
 
     @PostConstruct
     protected void init(){key = Base64.getEncoder().encodeToString(key.getBytes());
     }
 
     public TokenResponse createTokenByLogin(UserResponse userResponse) throws JsonProcessingException{
-        Subject atkSubject = Subject.atk(
-                userResponse.getId(),
-                userResponse.getEmail(),
-                userResponse.getAccountId());
-        String atk = createToken(atkSubject, atkTime);
-        return new TokenResponse(atk, null);
+        String atk = createToken(userResponse, atkTime, "atk");
+        String rtk = createToken(userResponse, rtkTime, "rtk");
+        redisDao.setValues(userResponse.getEmail(), rtk, Duration.ofMillis(rtkTime));
+        return new TokenResponse(atk, rtk);
     }
 
-    public Subject getSubject(String atk) throws JsonProcessingException{
-        String subjectStr = Jwts.parser().setSigningKey(key).parseClaimsJws(atk).getBody().getSubject();
-        return objectMapper.readValue(subjectStr, Subject.class);
+    // Jwt 토큰 인증 정보조회
+    public Authentication getAuthentication(String token){
+        // Jwt 에서 정보 추출
+        Claims claims = parseClaims(token);
+        // 권한 정보가 없음
+        if (claims.get("roles") == null){
+            log.error("YOUR_NOT_ROLE");
+        }
+        UserDetails userDetails = userDetailsService.loadUserByUsername(claims.getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
     }
 
-    private String createToken(Subject subject, Long tokenTime) throws JsonProcessingException{
-        String subjectStr = objectMapper.writeValueAsString(subject);
-        log.info("subjectStr = {} ", subjectStr);
-        Claims claims = Jwts.claims().setSubject(subjectStr);
+    // Jwt 토큰 복호화해서 가져오기
+    public Claims parseClaims(String token){
+        try {
+            return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+        }catch (ExpiredJwtException e){
+            return e.getClaims();
+        }
+    }
+
+    // Request의 Header 에서 token값을 가져옴. "Authorization" : "TOKEN 값"
+    public String resolveToken(HttpServletRequest request){
+        return request.getHeader("Authorization");
+    }
+
+    // 토큰의 유효성 + 만료일자 확인
+    public boolean validateToken(String jwtToken){
+        try {
+            Jwts.parser().setSigningKey(key).parseClaimsJws(jwtToken);
+            return true;
+        }catch (JwtException | IllegalArgumentException e){
+            log.error(e.toString());
+            return false;
+        }
+    }
+
+    public TokenResponse reissueAtk(UserResponse userResponse) throws JsonProcessingException {
+        String rtkInRedis = redisDao.getValues(userResponse.getEmail());
+        if (Objects.isNull(rtkInRedis)) throw new IllegalStateException("인증 정보가 만료되었습니다.");
+        return new TokenResponse(createToken(userResponse, atkTime, "atk"), null);
+    }
+
+    private String createToken(UserResponse userResponse, Long tokenTime, String type) throws JsonProcessingException{
+        Claims claims = Jwts.claims().setSubject(userResponse.getEmail());
+        claims.put("roles", userResponse.getRole());
         Date date = new Date();
         return Jwts.builder()
+                .claim("type", type)
                 .setClaims(claims)
                 .setIssuedAt(date)
                 .setExpiration(new Date(date.getTime() + tokenTime))
