d80c9413f03f18c44ed56242255b77d6817d7657
beargame123 <hoyoung7827@gmail.com>
2022-10-05 12:11:13 +0900

    🛠️:: All -> No

--- src/main/java/com/example/deconhubserver/domain/user/dto/LoginRequest.java
+++ src/main/java/com/example/deconhubserver/domain/user/dto/LoginRequest.java
@@ -1,11 +1,11 @@
 package com.example.deconhubserver.domain.user.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
-    private final String email;
-    private final String password;
+    private String email;
+    private String password;
 }
