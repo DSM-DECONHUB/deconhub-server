7d7e1fb1dfbfbdbd392cc480fc68c6c583ce039d
beargame123 <hoyoung7827@gmail.com>
2022-10-09 19:49:54 +0900

    🛠️:: findByEmail() -> findByAccountId() change

--- src/main/java/com/example/deconhubserver/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/example/deconhubserver/global/security/auth/AuthDetailsService.java
@@ -15,8 +15,8 @@ public class AuthDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email)
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        User user = userRepository.findByAccountId(accountId)
                 .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다."));
         return new AuthDetails(user);
     }
