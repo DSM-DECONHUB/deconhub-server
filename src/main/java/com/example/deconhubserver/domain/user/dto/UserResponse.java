0ac88b72341db5ef60210ff6be08e57c0b7eef54
beargame123 <hoyoung7827@gmail.com>
2022-10-11 23:36:32 +0900

    ☠️:: email delete

--- src/main/java/com/example/deconhubserver/domain/user/dto/UserResponse.java
+++ src/main/java/com/example/deconhubserver/domain/user/dto/UserResponse.java
@@ -2,21 +2,17 @@ package com.example.deconhubserver.domain.user.dto;
 
 import com.example.deconhubserver.domain.user.entity.User;
 import com.example.deconhubserver.domain.user.enums.Role;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class UserResponse {
-    private final String email;
-    private final String accountId;
-    private final Role role;
+    private String accountId;
+    private Role role;
 
     public static UserResponse of(User user){
         return UserResponse.builder()
-                .email(user.getEmail())
                 .accountId(user.getAccountId())
                 .role(user.getRole()).build();
     }
