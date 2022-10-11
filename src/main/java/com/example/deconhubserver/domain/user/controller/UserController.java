e34031d1f7ebbd9ec59d0edaea8eda9cc8c2bbbf
beargame123 <hoyoung7827@gmail.com>
2022-10-11 23:35:45 +0900

    ➕:: swagger logic add

--- src/main/java/com/example/deconhubserver/domain/user/controller/UserController.java
+++ src/main/java/com/example/deconhubserver/domain/user/controller/UserController.java
@@ -7,10 +7,14 @@ import com.example.deconhubserver.domain.user.service.UserService;
 import com.example.deconhubserver.global.security.auth.AuthDetails;
 import com.example.deconhubserver.global.security.jwt.JwtTokenProvider;
 import com.example.deconhubserver.global.security.jwt.dto.TokenResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(name = "user", description = "유저 관련 API 입니다.")
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
@@ -18,17 +22,22 @@ public class UserController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/signup")
-    public UserResponse login(@RequestBody SignupRequest request){
-        return userService.signup(request);
+    @ResponseStatus(HttpStatus.CREATED)
+    public void login(@RequestBody SignupRequest request){
+        userService.signup(request);
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
+    @ResponseStatus(HttpStatus.CREATED)
     public TokenResponse login(@RequestBody LoginRequest request){
         UserResponse userResponse = userService.login(request);
         return jwtTokenProvider.createTokenByLogin(userResponse);
     }
 
+    @Operation(summary = "토큰 재발급")
     @GetMapping("/reissue")
     public TokenResponse reissue(
             @AuthenticationPrincipal AuthDetails authDetails
