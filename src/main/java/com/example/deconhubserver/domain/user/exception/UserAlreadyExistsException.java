097127db0fc3ae92ce4aec3a98df793e9c6c1d74
beargame123 <hoyoung7827@gmail.com>
2022-10-19 11:31:04 +0900

    .

--- src/main/java/com/example/deconhubserver/domain/user/exception/UserAlreadyExistsException.java
+++ src/main/java/com/example/deconhubserver/domain/user/exception/UserAlreadyExistsException.java
@@ -2,7 +2,6 @@ package com.example.deconhubserver.domain.user.exception;
 
 import com.example.deconhubserver.global.error.ErrorCode;
 import com.example.deconhubserver.global.error.exception.CustomException;
-import com.mysql.cj.x.protobuf.Mysqlx;
 
 public class UserAlreadyExistsException extends CustomException {
     public static final CustomException EXCEPTION =
