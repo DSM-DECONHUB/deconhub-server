cf7b5ae537aabbc8ba89e88a16c02b29a0cad4d9
jaemin <sonjm3518@dsm.hs.kr>
2022-10-22 01:59:59 +0900

    ➕:: ImageFacade

---
+++ src/main/java/com/example/deconhubserver/infrastucture/s3/facade/ImageFacade.java
@@ -0,0 +1,61 @@
+package com.example.deconhubserver.infrastucture.s3.facade;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.example.deconhubserver.infrastucture.s3.ImageUtil;
+import com.example.deconhubserver.infrastucture.s3.exception.ImageNotFoundException;
+import com.example.deconhubserver.infrastucture.s3.exception.InvalidImageExtensionFormatException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+@Component
+public class ImageFacade implements ImageUtil {
+
+    @Value("${cloud.aws.s3.bucket}")
+    private final String bucket;
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Override
+    public String upload(MultipartFile multipartFile) {
+        String extension = getExtension(multipartFile);
+        String imageUrl = "${UUID.randomUUID()} $extension";
+
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(multipartFile.getSize());
+        objectMetadata.setContentType(multipartFile.getContentType());
+
+        try {
+            amazonS3Client.putObject(
+                    new PutObjectRequest(bucket, imageUrl, multipartFile.getInputStream(), objectMetadata)
+                            .withCannedAcl(CannedAccessControlList.PublicRead)
+            );
+        } catch (IOException e) {
+            throw ImageNotFoundException.EXCEPTION;
+        }
+
+        return amazonS3Client.getUrl(bucket, imageUrl).toString();
+    }
+
+    private String getExtension(MultipartFile multipartFile) {
+        if (multipartFile.isEmpty() || multipartFile.getOriginalFilename() == null) {
+            throw ImageNotFoundException.EXCEPTION;
+        }
+
+        String originalName = multipartFile.getOriginalFilename();
+        String extension = originalName.substring(originalName.lastIndexOf(".")).toLowerCase();
+
+        if (!(extension.equals(".jpg") || extension.equals(".jpeg") || extension.equals(".png") || extension.equals(".heic"))) {
+            throw InvalidImageExtensionFormatException.EXCEPTION;
+        }
+
+        return extension;
+    }
+}
