ce5746b96954588949b54e119071812ca30f7bd9
jaemin <sonjm3518@dsm.hs.kr>
2022-10-25 08:32:32 +0900

    ♻️ :: 오타 수정

--- src/main/java/com/example/deconhubserver/domain/image/controller/ImageController.java
+++ src/main/java/com/example/deconhubserver/domain/image/controller/ImageController.java
@@ -4,10 +4,7 @@ import com.example.deconhubserver.domain.image.dto.ImageListResponse;
 import com.example.deconhubserver.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -20,6 +17,7 @@ public class ImageController {
     private final ImageService imageService;
 
     @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
     public ImageListResponse upload(@RequestParam List<MultipartFile> images) {
         return imageService.imageUpload(images);
     }
