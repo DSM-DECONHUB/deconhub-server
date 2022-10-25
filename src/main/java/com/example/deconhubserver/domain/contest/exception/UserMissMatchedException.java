71ca73c5ed47fad6bf4925d4df99bd6a4e093687
beargame123 <hoyoung7827@gmail.com>
2022-10-25 13:28:08 +0900

    .

--- src/main/java/com/example/deconhubserver/domain/contest/exception/UserMissMatchedException.java
+++ src/main/java/com/example/deconhubserver/domain/contest/exception/UserMissMatchedException.java
@@ -7,8 +7,8 @@ public class UserMissMatchedException extends CustomException {
     public static final CustomException EXCEPTION =
             new UserMissMatchedException();
 
-
     private UserMissMatchedException() {
         super(ErrorCode.USER_MISS_MATCHED);
     }
+
 }
