9cfeb302f36ce058657ecca07e121b6635ed37b3
beargame123 <hoyoung7827@gmail.com>
2022-10-27 10:54:43 +0900

    🛠️:: swagger add

--- src/main/java/com/example/deconhubserver/domain/image/controller/ImageController.java
+++ src/main/java/com/example/deconhubserver/domain/image/controller/ImageController.java
@@ -2,6 +2,8 @@ package com.example.deconhubserver.domain.image.controller;
 
 import com.example.deconhubserver.domain.image.dto.ImageListResponse;
 import com.example.deconhubserver.domain.image.service.ImageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Tag(name = "image", description = "이미지 관련 API 입니다.")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/images")
@@ -16,6 +19,8 @@ public class ImageController {
 
     private final ImageService imageService;
 
+
+    @Operation(summary = "이미지 업로드")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ImageListResponse upload(@RequestParam List<MultipartFile> images) {
