cb4a9516e22766bba590b94c33ce052f53eea0fb
beargame123 <hoyoung7827@gmail.com>
2022-10-27 10:53:08 +0900

    🗑️:: ImageUtil del

--- src/main/java/com/example/deconhubserver/infrastucture/s3/ImageUtil.java
+++
@@ -1,7 +0,0 @@
-package com.example.deconhubserver.infrastucture.s3;
-
-import org.springframework.web.multipart.MultipartFile;
-
-public interface ImageUtil {
-    String upload(MultipartFile multipartFile);
-}
