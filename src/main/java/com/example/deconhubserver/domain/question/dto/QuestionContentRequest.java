01f4f2b7e07461d56e97522a224bf1abe79016c8
beargame123 <hoyoung7827@gmail.com>
2022-11-15 16:43:28 +0900

    ➕:: ContentRequest add

---
+++ src/main/java/com/example/deconhubserver/domain/question/dto/QuestionContentRequest.java
@@ -0,0 +1,10 @@
+package com.example.deconhubserver.domain.question.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class QuestionContentRequest {
+    private String content;
+}
