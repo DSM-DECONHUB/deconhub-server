3e4f647a68f719b05083b0d162e48a624ac239a6
beargame123 <hoyoung7827@gmail.com>
2022-11-15 23:48:11 +0900

    🛠️:: 변수 수정

--- src/main/java/com/example/deconhubserver/domain/question/service/QuestionService.java
+++ src/main/java/com/example/deconhubserver/domain/question/service/QuestionService.java
@@ -3,10 +3,7 @@ package com.example.deconhubserver.domain.question.service;
 import com.example.deconhubserver.domain.contest.entity.Contest;
 import com.example.deconhubserver.domain.contest.exception.UserMissMatchedException;
 import com.example.deconhubserver.domain.contest.facade.ContestFacade;
-import com.example.deconhubserver.domain.question.dto.QuestionContentRequest;
-import com.example.deconhubserver.domain.question.dto.QuestionList;
-import com.example.deconhubserver.domain.question.dto.QuestionTitleRequest;
-import com.example.deconhubserver.domain.question.dto.QuestionResponse;
+import com.example.deconhubserver.domain.question.dto.*;
 import com.example.deconhubserver.domain.question.entity.Question;
 import com.example.deconhubserver.domain.question.facade.QuestionFacade;
 import com.example.deconhubserver.domain.question.repository.QuestionRepository;
@@ -30,7 +27,7 @@ public class QuestionService {
     private final ContestFacade contestFacade;
 
     @Transactional
-    public void createQuestion(QuestionTitleRequest request, Long id){
+    public void createQuestion(QuestionTitleRequest request, Long id) {
         User user = userFacade.getCurrentUser();
         Contest contest = contestFacade.findById(id);
         Question question = new Question(
@@ -42,13 +39,13 @@ public class QuestionService {
     }
 
     @Transactional(readOnly = true)
-    public List<QuestionList> questionList(Long id){
+    public List<QuestionList> questionList(Long id) {
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
         List<Question> questions = questionFacade.findAllById(sort);
         List<QuestionList> questionLists = new ArrayList<>();
 
-        for(Question question : questions){
-            if(question.getContest().getId().equals(id)){
+        for (Question question : questions) {
+            if (question.getContest().getId().equals(id)) {
                 QuestionList dto = QuestionList.builder()
                         .title(question.getTitle()).build();
                 questionLists.add(dto);
@@ -58,7 +55,7 @@ public class QuestionService {
     }
 
     @Transactional(readOnly = true)
-    public QuestionResponse questionDetail(Long id){
+    public QuestionResponse questionDetail(Long id) {
         Question question = questionFacade.findById(id);
         return QuestionResponse.builder()
                 .title(question.getTitle())
@@ -66,21 +63,38 @@ public class QuestionService {
                 .accountId(question.getUser().getAccountId()).build();
     }
 
+    @Transactional(readOnly = true)
+    public QuestionContestList questionContestResponseList() {
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        List<Question> questions = questionFacade.findAllById(sort);
+        List<QuestionContestResponse> questionLists = new ArrayList<>();
+
+        for (Question question : questions) {
+            QuestionContestResponse dto = QuestionContestResponse.builder()
+                    .questionId(question.getId())
+                    .contestId(question.getContest().getId())
+                    .contestName(question.getContest().getTitle())
+                    .questionTitle(question.getTitle()).build();
+            questionLists.add(dto);
+        }
+        return new QuestionContestList(questionLists);
+    }
+
     @Transactional
-    public void modifyQuestion(Long id, QuestionTitleRequest request){
+    public void modifyQuestion(Long id, QuestionTitleRequest request) {
         Question question = questionFacade.findById(id);
         User user = userFacade.getCurrentUser();
-        if (!question.getUser().getId().equals(user.getId())){
+        if (!question.getUser().getId().equals(user.getId())) {
             throw UserMissMatchedException.EXCEPTION;
         }
         question.setQuestion(request.getTitle());
     }
 
     @Transactional
-    public void questionAnswer(Long id, QuestionContentRequest request){
+    public void questionAnswer(Long id, QuestionContentRequest request) {
         Question question = questionFacade.findById(id);
         User user = userFacade.getCurrentUser();
-        if(!user.getRole().equals(Role.ADMIN)){
+        if (!user.getRole().equals(Role.ADMIN)) {
             throw UserMissMatchedException.EXCEPTION;
         }
         question.answerQuestion(request.getContent());
